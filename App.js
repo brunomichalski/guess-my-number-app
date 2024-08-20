d803197b9dbbb6ac2fbac96e9e9843e43d3882a5
Bruno Michalski <bmichalski@coamo.com.br>
2024-08-20 08:09:16 -0300

    feat: ajustando view de itens e background

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import StartGameScreen from './src/screens/StartGameScreen'
+
+import { StyleSheet} from "react-native";
+import StartGameScreen from "./src/screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
+    <LinearGradient colors={['#3b021f', "#ddb52f"]} style={styles.rootScreen}>
       <StartGameScreen />
+    </LinearGradient>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  rootScreen: {
+    flex: 1
   },
 });
--- src/screens/StartGameScreen.js
+++ src/screens/StartGameScreen.js
@@ -1,36 +1,55 @@
 import { View, TextInput, Text, StyleSheet } from "react-native";
-import PrimaryButton from "../components/PrimaryButton"
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    return (
-        <View style={style.inputContainer}>
-            <TextInput style={style.textInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
-        </View>);
+  return (
+    <View style={style.inputContainer}>
+      <TextInput
+        style={style.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+      />
+      <View style={style.buttonsContainer}>
+        <View style={style.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={style.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 export default StartGameScreen;
 
 const style = StyleSheet.create({
-    inputContainer: {
-        marginHorizontal: 24,
-        padding: 16,
-        marginTop: 100,
-        backgroundColor: "#4e0329",
-        borderRadius: 8,
-        elevation: 10
-    },
-    textInput: {
-        height: 50,
-        width:50, 
-        fontSize: 32,
-        borderBottomColor: '#ddb52f',
-        borderBottomWidth: 2,
-        color:'#ddb52f',
-        marginVertical: 8,
-        fontWeight: 'bold ',
-        textAlign: 'center',
-        
-    }
-})
+  inputContainer: {
+    // justifyContent: 'center',
+    alignItems: "center",
+    marginHorizontal: 24,
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 10,
+  },
+  textInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold ",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
